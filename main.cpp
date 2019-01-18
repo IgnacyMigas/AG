f0c6f7cb8ccf4ddb8a34180578b4f1f1f46c3ee8
IgnacyMigas <gigi.migas@gmail.com>
2019-01-18 15:13:17 +0100

    Code style improve

--- main.cpp
+++ main.cpp
@@ -18,11 +18,9 @@ float pmut   = 1.0;
 float pcross = 0.0;
 
 const int MAX_NR = 1003;
-float v_min=51, v_max=-1, p_max=1;
+float p_max=1;
 float prenty[MAX_NR];
 int len=0;
-
-float odchylenie_max=0;
 
 float objective(GAGenome &);
 
@@ -40,9 +38,9 @@ int main(int argc, char *argv[])
 	float v;
 	float m1=0, m2=0, m3=0;
 	unsigned seed = time(0);
-	
+
 	ifstream we("prety.txt");
-	
+
 	while(!we.eof()){
 		we>>v;
 		we>>v;
@@ -59,44 +57,39 @@ int main(int argc, char *argv[])
 		else if (v>m3){
 			m3=v;
 		}
-		
-		if (v<v_min) v_min = v;
-		if (v>v_max) v_max = v;
 	}
-	
-	
+
 	we.close();
 
-	
 	p_max = pole(m1, m2, m3)/2.;
 
-  GAListGenome<int> genome(Objective);
-  genome.initializer(::Initializer);
-  genome.mutator(::Mutator);
-  genome.comparator(::Comparator);
+	GAListGenome<int> genome(Objective);
+	genome.initializer(::Initializer);
+	genome.mutator(::Mutator);
+	genome.comparator(::Comparator);
 
-  GASteadyStateGA ga(genome);
-  ga.minimize();
-  ga.pReplacement(1.0);
-  ga.populationSize(popsize);
-  ga.nGenerations(ngen);
-  ga.pMutation(pmut);
-  ga.pCrossover(pcross);
-  ga.selectScores(GAStatistics::AllScores);
-  ga.parameters(argc, argv);
-  ga.initialize(seed);
-  while(!ga.done()) {
-    ga.step();
-  }
+	GASteadyStateGA ga(genome);
+	ga.minimize();
+	ga.pReplacement(1.0);
+	ga.populationSize(popsize);
+	ga.nGenerations(ngen);
+	ga.pMutation(pmut);
+	ga.pCrossover(pcross);
+	ga.parameters(argc, argv);
+	ga.initialize(seed);
 
-  genome = ga.statistics().bestIndividual();
-  float od = 0;
-  int cnt = 0;
-  licz(genome, cnt, od);
-  cout<< "\n\nOdchylenie=" << od << ", ilosc=" << cnt << endl<<endl;
-  save_output(genome);
+	while(!ga.done()) {
+		ga.step();
+	}
 
-  return 0;
+	genome = ga.statistics().bestIndividual();
+	save_output(genome);
+	float od = 0;
+	int cnt = 0;
+	licz(genome, cnt, od);
+	cout << endl << "Odchylenie=" << od << ", ilosc=" << cnt << endl << endl;
+
+	return 0;
 }
 
 float Objective(GAGenome & g)
@@ -104,83 +97,83 @@ float Objective(GAGenome & g)
 	float odchylenie = 0;
 	int cnt = 0;
 	licz(g, cnt, odchylenie);
-  
-  float fitness = (len/3.-cnt)+(odchylenie/p_max*(len*1.5));
-  if (fitness != fitness)
-	  return len*100;
-  return fitness;
+
+	float fitness = (len/3.-cnt)+(odchylenie/p_max*(len*1.5));
+	if (fitness != fitness)
+		return len*100;
+	return fitness;
 }
 
 void Initializer(GAGenome& g) {
-  GAListGenome<int> &child=(GAListGenome<int> &)g;
-  while(child.head()) child.destroy(); // destroy any pre-existing list
-  
-  child.insert(0,GAListBASE::HEAD);
-  for (int i=1; i<len; ++i){
-	  child.insert(i);
-  }
+	GAListGenome<int> &child=(GAListGenome<int> &)g;
+	while(child.head()) child.destroy(); // destroy any pre-existing list
+
+	child.insert(0,GAListBASE::HEAD);
+	for (int i=1; i<len; ++i){
+		child.insert(i);
+	}
 }
 
 int Mutator(GAGenome& g, float pmut) {
-  GAListGenome<int> &child=(GAListGenome<int> &)g;
-  register int n, i;
-  if ((GARandomFloat() >= pmut) || (pmut <= 0)) return 0;
+	GAListGenome<int> &child=(GAListGenome<int> &)g;
+	register int n, i;
+	if ((GARandomFloat() >= pmut) || (pmut <= 0)) return 0;
 
-  n = child.size();
-  
-  if (GARandomFloat()<0.5) {
-    child.swap(GARandomInt(0,n-1),GARandomInt(0,n-1)); // swap only one time
-  }
-  else {
-    int nNodes = GARandomInt(1,((int)(n/2-1)));       // displace nNodes 
-    child.warp(GARandomInt(0,n-1));                   // with or without
-    GAList<int> TmpList;                              // inversion
-    for(i=0;i<nNodes;i++) {
-      int *iptr = child.remove();
-      TmpList.insert(*iptr,GAListBASE::AFTER);
-      delete iptr;
-      child.next();
-    }
-    int invert;
-    child.warp(GARandomInt(0,n-nNodes));
-    invert = (GARandomFloat()<0.5) ? 0 : 1;
-    if (invert) TmpList.head(); else TmpList.tail();
+	n = child.size();
 
-    for(i=0;i<nNodes;i++) {
-      int *iptr = TmpList.remove();
-      child.insert(*iptr,GAListBASE::AFTER);
-      delete iptr;
-      if (invert) TmpList.prev(); else TmpList.next();
-    }
-  }
-  child.head();		// set iterator to root node
+	if (GARandomFloat()<0.5) {
+		child.swap(GARandomInt(0,n-1),GARandomInt(0,n-1)); // swap only one time
+	}
+	else {
+		int nNodes = GARandomInt(1,((int)(n/2-1)));       // displace nNodes 
+		child.warp(GARandomInt(0,n-1));                   // with or without
+		GAList<int> TmpList;                              // inversion
+		for(i=0;i<nNodes;i++) {
+			int *iptr = child.remove();
+			TmpList.insert(*iptr,GAListBASE::AFTER);
+			delete iptr;
+			child.next();
+		}
+		int invert;
+		child.warp(GARandomInt(0,n-nNodes));
+		invert = (GARandomFloat()<0.5) ? 0 : 1;
+		if (invert) TmpList.head(); else TmpList.tail();
 
-  return (1);
+		for(i=0;i<nNodes;i++) {
+			int *iptr = TmpList.remove();
+			child.insert(*iptr,GAListBASE::AFTER);
+			delete iptr;
+			if (invert) TmpList.prev(); else TmpList.next();
+		}
+	}
+	child.head();		// set iterator to root node
+
+	return (1);
 }
 
 float Comparator(const GAGenome& g1, const GAGenome& g2) {
-  GAListGenome<int> &a = (GAListGenome<int> &)g1;
-  GAListGenome<int> &b = (GAListGenome<int> &)g2;
+	GAListGenome<int> &a = (GAListGenome<int> &)g1;
+	GAListGenome<int> &b = (GAListGenome<int> &)g2;
 
-  int sum=0;
-  for (int i=0; i<len; ++i){
-	  if (*a.current() != *b.current())
-		  sum++;
-  }
-  return sum;
+	int sum=0;
+	for (int i=0; i<len; ++i){
+		if (*a.current() != *b.current())
+			sum++;
+	}
+	return sum;
 }
 
 template <> int GAListGenome<int>::write(ostream & os) const
 {
-  int *cur, *head;
-  GAListIter<int> tmpiter(*this);
-  if((head=tmpiter.head()) != 0) {
-    os << *head+1 << " " << prenty[*head]<<endl;
-    for(cur=tmpiter.next(); cur && cur != head; cur=tmpiter.next())
-      os << *cur+1 << " " << prenty[*cur]<<endl;
-  }
+	int *cur, *head;
+	GAListIter<int> tmpiter(*this);
+	if((head=tmpiter.head()) != 0) {
+		os << *head+1 << " " << prenty[*head]<<endl;
+		for(cur=tmpiter.next(); cur && cur != head; cur=tmpiter.next())
+			os << *cur+1 << " " << prenty[*cur]<<endl;
+	}
 
-  return os.fail() ? 1 : 0;
+	return os.fail() ? 1 : 0;
 }
 
 float pole(float a, float b, float c){
@@ -189,44 +182,39 @@ float pole(float a, float b, float c){
 }
 
 void licz(GAGenome & g, int& cnt, float& odchylenie){
-  GAListGenome<int> & genome = (GAListGenome<int> &)g;
-  float sum=0;
-  float tab[MAX_NR/3];
-  if(genome.head()) {
-    for(int i=0; i<len/3; i++) {
-      float a = prenty[*genome.current()];
-      float b = prenty[*genome.next()];
-      float c = prenty[*genome.next()];
-	  genome.next();
-      if (a+b>c && a+c>b && b+c>a){
-		  tab[cnt]=pole(a, b, c);
-		  sum+=tab[cnt];
-		  cnt++;
-	  }
-    }
-	float avg = sum/cnt;
-	sum = 0;
-	for(int i=0; i<cnt; i++) {
-		sum += pow((tab[i]-avg), 2);
+	GAListGenome<int> & genome = (GAListGenome<int> &)g;
+	float sum=0;
+	float tab[MAX_NR/3];
+	if(genome.head()) {
+		for(int i=0; i<len/3; i++) {
+			float a = prenty[*genome.current()];
+			float b = prenty[*genome.next()];
+			float c = prenty[*genome.next()];
+			genome.next();
+			if (a+b>c && a+c>b && b+c>a){
+				tab[cnt]=pole(a, b, c);
+				sum+=tab[cnt];
+				cnt++;
+			}
+		}
+		float avg = sum/cnt;
+		sum = 0;
+		for(int i=0; i<cnt; i++) {
+			sum += pow((tab[i]-avg), 2);
+		}
+		odchylenie=sqrt(sum/cnt);
 	}
-	odchylenie=sqrt(sum/cnt);
-	if (odchylenie>odchylenie_max) {
-		odchylenie_max=odchylenie;
-	}
-  }
 }
 
 void save_output(const GAGenome& g) {
 	GAListGenome<int> & genome = (GAListGenome<int> &)g;
 
 	ofstream wy("output.txt");
-	
+
 	wy << *genome.head()+1 << endl;
 	for (int i=1; i<len; ++i){
 		wy << *genome.next()+1 << endl;
 	}
-	
+
 	wy.close();
 }
-
-
